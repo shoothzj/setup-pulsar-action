18387d5b19d2c4be094755f1eb166fa9904ad137
ZhangJian He <shoothzj@gmail.com>
2023-05-26 20:52:05 +0800

    adapt pulsar prefix env

--- start_pulsar.sh
+++ start_pulsar.sh
@@ -7,7 +7,8 @@ for var in "${!PULSAR_PREFIX_@}"; do
     cmd1+=" -e ${var}=${!var}"
 done
 
-docker run --name pulsar -p 6650:6650 -p 8080:8080 -d apachepulsar/pulsar:2.10.1 /pulsar/bin/pulsar standalone --no-functions-worker --no-stream-storage
-
 final_cmd="${cmd1} ${cmd2}"
+
+echo "Running command: $final_cmd"
+
 eval $final_cmd
